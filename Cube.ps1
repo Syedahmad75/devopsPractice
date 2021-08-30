343a16b0212044eab9cfa8c498828d06992cc502
syedahmad75 <syedahmad75@hotmail.com>
2021-08-30 14:10:08 +0200

    First Commit 08-30-2021

---
+++ Cube.ps1
@@ -0,0 +1,7 @@
+﻿clear-host  
+$Value1 = "" 
+$Answer = ""
+$Value1 = read-host " Enter the value "
+Write-Host " Cube of your value is=:"
+$Answer= [int]$Value1 * [int]$Value1 * [int]$Value1
+$Answer
