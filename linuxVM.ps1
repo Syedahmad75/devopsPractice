34e3a5f97f495b0e6592bb6273724c0c2de35fd6
syedahmad75 <syedahmad75@hotmail.com>
2021-09-06 21:34:18 +0200

    09-06-2021

---
+++ linuxVM.ps1
@@ -0,0 +1,14 @@
+﻿Clear-Host
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
+$adminUsername = Read-Host -Prompt "Enter the administrator username"
+$sshKey = Read-Host -Prompt "Enter the SSH public key"
+
+New-AzResourceGroup -Name $resourceGroupName -Location "$location"
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile "C:\Users\SyedShah\AzurePractise\linuxVm.txt" `
+    -adminUsername $adminUsername `
+    -adminPublicKey $sshKey
+
+ (Get-AzVm -ResourceGroupName $resourceGroupName).name
---
+++ windowVM.ps1
@@ -0,0 +1,16 @@
+﻿Clear-Host
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
+$adminUsername = Read-Host -Prompt "Enter the administrator username"
+$adminPassword = Read-Host -Prompt "Enter the administrator password" -AsSecureString
+$dnsLabelPrefix = Read-Host -Prompt "Enter an unique DNS name for the public IP"
+
+New-AzResourceGroup -Name $resourceGroupName -Location "$location"
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile "C:\Users\SyedShah\AzurePractise\vmDeploy.json" `
+    -adminUsername $adminUsername `
+    -adminPassword $adminPassword `
+    -dnsLabelPrefix $dnsLabelPrefix
+
+ (Get-AzVm -ResourceGroupName $resourceGroupName).name
