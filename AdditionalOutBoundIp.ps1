be13df88583d82db40879db744fac80db3eeb947
syedahmad75 <syedahmad75@hotmail.com>
2021-09-15 08:30:00 +0200

    09-15-2021

---
+++ AdditionalOutBoundIp.ps1
@@ -0,0 +1,5 @@
+﻿Clear-Host
+[string[]]$temp=@((Get-AzWebApp).PossibleOutboundIpAddresses.Split("{,}")) | select -Unique 
+$count=$temp.Count
+Write-Host "Total Number of Unique Additional Outbound Addresses: $count 'n"
+$temp
---
+++ FolderFilesTask.ps1
@@ -0,0 +1,27 @@
+﻿Clear-Host
+$sourcePath= Read-Host "Please Enter the Path:"
+$hostPath= Read-Host "Enter the Dest Path:"
+
+
+#To Create New Folder with Date and Time
+$date=Get-Date -Format "MM-dd-yyyy-HHmm"
+new-item $hostPath  -Name $date  -itemtype directory -Force
+$hostNewPath="$hostPath"+ "\" + "$date"
+
+
+#Get Length and Files from Source Folder
+$Files=@(Get-Childitem –Path $sourcePath  -Recurse)
+$length=$files.Length
+
+#Main Loop to separate Files
+for($i=0; $i -le $length; $i++)
+{
+   $temp=$files[$i].Extension
+   $foldertype=$temp.Trim(".")
+ 
+   $destPath=new-item $hostNewPath  -Name $foldertype  -itemtype directory -Force
+
+   $destPath= "$destPath" + "\"+"$foldertype"
+   Get-Childitem –Path $sourcePath  -Include *$temp* -Recurse | Move-Item -Destination $destPath
+   Remove-Item $sourcePath
+}
